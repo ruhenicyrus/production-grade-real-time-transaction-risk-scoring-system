76a8e97c02204cddf3f570d3b4744dfdad2fe659
Cyrus Ruheni <116971536+ruhenicyrus@users.noreply.github.com>
2026-01-06 18:19:32 +0300

    Pushing the SQLite3 database into the repo

--- risk_scoring/risk_level.py
+++ risk_scoring/risk_level.py
@@ -1,17 +1,39 @@
-#checking the level of transaction risk
+import sqlite3
+
+connection = sqlite3.connect("../transactions.db")
+cursor = connection.cursor()
+
 def checking_transaction_risk(transaction):
-	if transaction["amount"]>500:
-		return "VERY HIGH"
-	elif transaction["amount"]>300:
-		return"HIGH"
-	else:
-		return"LOW"
+    if transaction["amount"] > 500:
+        return "VERY HIGH"
+    elif transaction["amount"] > 300:
+        return "HIGH"
+    else:
+        return "LOW"
 
 transactions = [
     {"id": 1, "amount": 100},
     {"id": 2, "amount": 350},
     {"id": 3, "amount": 800}
 ]
+
 for txn in transactions:
-	risk=checking_transaction_risk(txn)
-	print("Transaction ID",txn["id"],"Risk is",risk)
+    risk = checking_transaction_risk(txn)
+
+    cursor.execute(
+        "INSERT INTO transactions (amount, risk_level) VALUES (?, ?)",
+        (txn["amount"], risk)
+    )
+
+    print(
+        "Transaction ID",
+        txn["id"],
+        "Amount",
+        txn["amount"],
+        "Risk",
+        risk,
+        "→ STORED"
+    )
+
+connection.commit()
+connection.close()
